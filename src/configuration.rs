3427a3609279ca912a0aa70ad2315b8fd81f4592
Johannes <jojurg@ttu.ee>
2024-11-22 11:48:54 +0200

    configuration settings

---
+++ src/configuration.rs
@@ -0,0 +1,25 @@
+//! src/configuration.rs
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize, Clone)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // initialize configuration reader
+    let settings = config::Config::builder()
+        // add configuration value from a file named 'configuration.yaml'
+        .add_source(config::File::new("configuration.yaml", config::FileFormat::Yaml))
+        .build()?;
+    // try to convert read settings into a Config type
+    settings.try_deserialize::<Settings>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // panic if we don't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
     // Bubble up the error if we failed to bind the address
     // otherwise call .await on our server
-    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
