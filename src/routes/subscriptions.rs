b0d4ae88076166f117adb6ff20f050b4929f0f2d
Johannes <jojurg@ttu.ee>
2024-12-20 13:29:13 +0200

    original context into tracing done

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[allow(dead_code)]
@@ -33,10 +34,10 @@ pub async fn subscribe(
     // don't try at home...
     let _request_info_guard = request_span.enter();
 
-    tracing::info!(
-        "request_id: {} - Saving new subscriber info to database",
-        request_id
-    );
+    // we don't call enter on query_span, instrument takes
+    // care of that for us in the right moments in the futures lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -50,19 +51,18 @@ pub async fn subscribe(
     // We use get_ref() to get an immutable reference to the 'PgConnection'
     // wrapped by 'web::data'
     .execute(pool.get_ref())
+    .instrument(query_span)
+    // first we attach the instrumentation, then we `await` it
     .await
     {
         Ok(_) => {
-            tracing::info!(
-                "request_id: {} - New subscriber details have been saved in the database",
-                request_id
-            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
+            // this currently falls outside of `query_span`
+            // we'll rectify it later
             tracing::error!(
-                "request_id: {} - Failed to execute query: {:?}",
-                request_id,
+                "Failed to execute query: {:?}",
                 e
             );
             HttpResponse::InternalServerError().finish()
