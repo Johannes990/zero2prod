1211f0264f59cc486f0793082a7a7b80707a6a1d
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-12-22 15:06:11 +0200

    refactoring to use telemetry in tests, also set some env variables for tests to show tracing output in stdout

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Rust project workthrough by Johannes Jyrgenson
 ### running with trace level logs on powershell terminal
 use `$env:RUST_LOG="trace"; cargo run` as the run command to get traces displayed in info logs
 
+### running tests with trace logs
+use `$env:TEST_LOG="true"; cargo test | bunyan`. Here bunyan is optional to get formatting prettier
+
 ## Issues
 
 ### sqlx prepare is missing one or more queries
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,7 +7,33 @@
 //! `cargo expand --test health_check` (<- name of the test file)
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use std::sync::LazyLock;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: LazyLock<()> = LazyLock::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    // We cannot assign the output of `get_subscriber` to a variable based on the
+    // value `TEST_LOG` because the sink is part of the type returned by
+    // `get_subscriber`, therfore they are not the same type. We could work around
+    // it, but this is the most straight forward way of moving forward.
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::stdout
+        );
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::sink
+        );
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -19,6 +45,10 @@ pub struct TestApp {
 // If we fail to perform the required setup we can just panic and crash
 // all the things.
 async fn spawn_app() -> TestApp {
+    // The first time we `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    LazyLock::force(&TRACING);
+
     // bind to port 0, which is a special case.
     // Port 0 triggers the OS to search for an available port which to bind to
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
