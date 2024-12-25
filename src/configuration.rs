15b880b6c91721c01abdc43f41b134f8b4c2fb34
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-12-25 20:00:30 +0200

    Add database configs and support for env vars in settings
    Introduced database configuration to `spec.yaml` and updated the application to support environment variable overrides for settings. Added `serde-aux` as a dependency to facilitate deserialization of numeric strings, improving configuration handling.

--- src/configuration.rs
+++ src/configuration.rs
@@ -11,6 +11,7 @@ pub struct Settings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: SecretString,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -18,6 +19,7 @@ pub struct DatabaseSettings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -56,6 +58,14 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(environment_filename),
         ))
+        // Add in settings from environment variables (with a prefix of APP
+        // and '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001` would set `Settings.application.port`
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__")
+        )
         .build()?;
 
     settings.try_deserialize::<Settings>()
