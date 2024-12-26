23a4a7de6a67eef1c9882b04b0620c64ffa44c2e
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-12-26 18:45:34 +0200

    Add subscriber name and email validation functionalities
    Introduced `SubscriberName` and `SubscriberEmail` structs with parsing and validation logic to ensure proper handling of inputs. Updated subscription route handler to validate incoming data and modified tests to reflect changes, rejecting invalid inputs with a `400 Bad Request` status. Added necessary dependencies for validation and unicode handling.

---
+++ src/domain/mod.rs
@@ -0,0 +1,9 @@
+//! src/domain/mod.rs
+
+mod subscriber_name;
+mod subscriber_email;
+mod new_subscriber;
+
+pub use subscriber_name::SubscriberName;
+pub use subscriber_email::SubscriberEmail;
+pub use new_subscriber::NewSubscriber;
