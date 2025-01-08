90b9f0132384c9b00b4a9ca00a186bc1d7043d45
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2025-01-08 16:07:33 +0200

    formatting fixes

--- src/email_client.rs
+++ src/email_client.rs
@@ -102,8 +102,11 @@ mod tests {
         // Arrange
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client =
-            EmailClient::new(mock_server.uri().as_str(), sender, SecretString::from(Faker.fake::<String>()));
+        let email_client = EmailClient::new(
+            mock_server.uri().as_str(),
+            sender,
+            SecretString::from(Faker.fake::<String>())
+        );
         let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let subject: String = Sentence(1..2).fake();
         let content: String = Paragraph(1..10).fake();
@@ -129,8 +132,11 @@ mod tests {
     async fn send_email_fails_if_the_server_returns_500() {
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client =
-            EmailClient::new(mock_server.uri().as_str(), sender, SecretString::from(Faker.fake::<String>()));
+        let email_client = EmailClient::new(
+            mock_server.uri().as_str(),
+            sender,
+            SecretString::from(Faker.fake::<String>())
+        );
 
         let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let subject: String = Sentence(1..2).fake();
@@ -154,8 +160,11 @@ mod tests {
         // Arrange
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client =
-            EmailClient::new(mock_server.uri().as_str(), sender, SecretString::from(Faker.fake::<String>()));
+        let email_client = EmailClient::new(
+            mock_server.uri().as_str(),
+            sender,
+            SecretString::from(Faker.fake::<String>())
+        );
 
         // Mock::given(any()) matches all incoming requests, regardless of
         // their method, path, headers or body. Verifies a request to the server without any
@@ -164,7 +173,7 @@ mod tests {
             .and(header("Content-Type", "application/json"))
             .and(path("/email"))
             .and(method(Method::POST))
-            .and(SendEmailBodyMatcher)  // Use our custom matcher
+            .and(SendEmailBodyMatcher) // Use our custom matcher
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
