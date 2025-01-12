681dabe282ff05df25474445f1d1fa0c6127241e
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2025-01-12 20:20:01 +0200

    refactored `subscribe_returns_200_for_valid_form_data()` test into two parts, first part checks only the 200 return code, second part: `subscribe_persists_new_subscriber` checks that the persisted data is existent and correct. also refactored `insert_subscricer` function now to insert a subscriber with status 'pending_confirmation' instead of 'success'

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -21,14 +21,31 @@ async fn subscribe_returns_200_for_valid_form_data() {
 
     // assert
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    // Act
+    test_app.post_subscriptions(body.into()).await;
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&test_app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
