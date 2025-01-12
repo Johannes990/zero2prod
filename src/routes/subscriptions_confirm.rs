0f30e206e7c1421a156981aaf25acc996946c27d
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2025-01-12 20:53:58 +0200

    tests now pass

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,10 +1,21 @@
 //! src/routes/subscriptions_confirm.rs
 
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, web};
+
+// This `Parameters` struct defines all the values we expect to see in
+// the incoming request.
+#[derive(serde::Deserialize)]
+pub struct Parameters {
+    subscription_token: String
+}
 
 #[tracing::instrument(
     name = "Confirm a pending subscriber",
+    skip(_parameters)
 )]
-pub async fn confirm() -> HttpResponse {
+// actix-web will only call handler if the extraction was successful
+// it is enough to add parameters to our `confirm()` method parameters
+// for actix-web to work.
+pub async fn confirm(_parameters: web::Query<Parameters>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
