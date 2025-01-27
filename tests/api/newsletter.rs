08b5d0019aed5fe800ee338af59ab62e22348c07
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2025-01-27 21:55:17 +0200

    TDD - made an integration test to check for delivery of newsletters to confirmed subscribers, the implementation is not yet done, so the test fails atm

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,6 +1,6 @@
 //! tests/api/newsletter.rs
 
-use crate::helpers::{spawn_app, TestApp};
+use crate::helpers::{spawn_app, ConfirmationLinks, TestApp};
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -38,9 +38,42 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     // Mock verifies on Drop that we haven't sent the newsletter email
 }
 
+#[tokio::test]
+async fn newsletters_are_delivered_to_confirmed_subscribers() {
+    // Arrange
+    let test_app = spawn_app().await;
+    create_confirmed_subscriber(&test_app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&test_app.email_server)
+        .await;
+
+    // Act
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>",
+        }
+    });
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &test_app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 200);
+    // Mock verifies on Drop that we have sent the newsletter email
+}
+
 /// Use the public API of the application under test tu create
 /// an unconfirmed subscriber
-async fn create_unconfirmed_subscriber(test_app: &TestApp) {
+async fn create_unconfirmed_subscriber(test_app: &TestApp) -> ConfirmationLinks {
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     let _mock_guard = Mock::given(path("/email"))
@@ -53,6 +86,28 @@ async fn create_unconfirmed_subscriber(test_app: &TestApp) {
     test_app
         .post_subscriptions(body.into())
         .await
+        .error_for_status()
+        .unwrap();
+
+    // We now inspect the requests received by the mock Postmark server
+    // to retrieve the confirmation link and return it
+    let email_request = &test_app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+
+    test_app.get_confirmation_links(&email_request)
+}
+
+async fn create_confirmed_subscriber(test_app: &TestApp) {
+    // just an extra call here to the confirmation link
+    let confirmation_link = create_unconfirmed_subscriber(test_app).await;
+    reqwest::get(confirmation_link.html)
+        .await
+        .unwrap()
         .error_for_status()
         .unwrap();
 }
