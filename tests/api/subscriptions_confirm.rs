dfc1c31735efcb187484bcf600e8d921a3224e76
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2025-01-27 20:45:41 +0200

    new test in `tests./api/subscriptions_confirm.rs` to check for broken database handling in `confirm()` method. Return value is HttpResponse::500

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -103,3 +103,31 @@ async fn get_subscription_id_from_token_fails_with_incorrect_token() {
 
     assert_eq!(response.status().as_u16(), 401);
 }
+
+#[tokio::test]
+async fn subscription_confirm_returns_500_when_no_status_field_present() {
+    let test_app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    test_app.post_subscriptions(body.into()).await;
+    let email_request = &test_app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()[0];
+    let confirmation_links = test_app.get_confirmation_links(&email_request);
+
+    sqlx::query!("ALTER TABLE subscriptions DROP COLUMN status;")
+        .execute(&test_app.db_pool)
+        .await
+        .unwrap();
+
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
+
+    assert_eq!(response.status().as_u16(), 500);
+}
